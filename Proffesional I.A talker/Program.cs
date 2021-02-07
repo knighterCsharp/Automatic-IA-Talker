bcf2238418f50d0728751902b0f4fc93255e5a56
knighterCsharp <78627412+knighterCsharp@users.noreply.github.com>
2021-02-06 21:13:41 -0500

    Add files via upload

---
+++ Proffesional I.A talker/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Brackeys_Tutorial._NEW_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "I.A talker";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello! What's ya name?");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Nice to meet you! I'm FI-001.\n I'm a friendly, non lethal A.I, I was made to say the same words every time.");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("And Who are you?");
+            Console.ReadLine();
+
+            Console.WriteLine("That's Cool! What is your favorite color?");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Okay Okay, Enough talk. In real.\nI'm a programmed Keanu Reeves and I can tell your color matches with this video: \n https://youtu.be/dQw4w9WgXcQ It's a nice surprise!");
+            Console.WriteLine("Did you saw it?");
+            Console.ReadLine();
+
+            Console.WriteLine("For real?");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Get rickrolled lol \twanna say something?");
+            Console.ReadLine();
+
+            Console.WriteLine("Guess i'll die. Close this uhhh thing with any key. See ya later, if you open me i'll recognize you as another human to rickroll m8");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
